7ec606cd6ded50715ca048115d0d1c3266325187
ErnestoZeferinoDiaz <ernesto.zefe.d@gmail.com>
2020-02-19 00:11:36 -0600

    comentando burbuja

--- burbuja.cpp
+++ burbuja.cpp
@@ -3,11 +3,14 @@
 #include <cstdlib>
 using namespace std;
 
+// funcion de intercambio
 void swap(int *a,int *b){
     int tmp=*a;
     *a=*b;
     *b=tmp;
 }
+
+// funcion comparador
 int comparator(int x,int y){
     if(x==y){
         return 0;
